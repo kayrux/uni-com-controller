6904310c05b21e71689bec6ff1f7c9f981985514
Surya Kusjanto <skusjanto@outlook.com>
2024-03-07 23:34:43 -0700

    Added styling and basic state changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WebSocketService } from './services/web-socket.service';
-
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+type State = 'selection' | 'controller' | 'bot';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'uni-com-controller';
+  public currentState: State = 'selection';
   public constructor(private webSocketService: WebSocketService) {}
 
   ngOnInit() {}
@@ -18,11 +21,17 @@ export class AppComponent {
   connectAsController() {
     console.log('connecting as controller...');
     this.webSocketService.connect('controller');
+    this.currentState = 'controller';
   }
 
   connectAsBot() {
     console.log('connecting as bot...');
     this.webSocketService.connect('bot');
+    this.currentState = 'bot';
+  }
+
+  returnToSelection() {
+    this.currentState = 'selection';
   }
 
   helloWorld() {
